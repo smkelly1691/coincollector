8f7cc22d5e524d8a8a48186f8eff3a811643767f
smkelly1691 <skelly1691@gmail.com>
2022-10-24 14:46:46 -0700

    adds class-based views

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('coins/', views.coins_index, name='index'),
-    path('coins/<int:coin_id>/', views.coins_detail, name='detail')
+    path('coins/<int:coin_id>/', views.coins_detail, name='detail'),
+    path('coins/create/', views.CoinCreate.as_view(), name ='coins_create'),
+    path('coins/<int:pk>/update/', views.CoinUpdate.as_view(), name='coins_update'),
+    path('coins/<int:pk>/delete/', views.CoinDelete.as_view(), name='coins_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Coin
 
 # Create your views here.
@@ -15,3 +16,16 @@ def coins_index(request):
 def coins_detail(request, coin_id):
     coin = Coin.objects.get(id=coin_id)
     return render(request, 'coins/detail.html', { 'coin': coin})
+
+class CoinCreate(CreateView):
+  model = Coin
+  fields = '__all__'
+#   success_url = '/coins/'
+
+class CoinUpdate(UpdateView):
+    model = Coin
+    fields = ['year', 'description', 'value']
+    
+class CoinDelete(DeleteView):
+    model = Coin
+    success_url = '/coins/'
