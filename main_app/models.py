2c09c9a10bf186635d35dcae4b958d2259631cd0
smkelly1691 <skelly1691@gmail.com>
2022-10-24 11:12:37 -0700

    adds Coin class and data via interactive shell

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Coin(models.Model):
+    name = models.CharField(max_length=100)
+    year = models.IntegerField()
+    description = models.TextField(max_length=250)
+    value = models.DecimalField(max_digits=7, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-class Coin: 
-    def __init__(self, name, year, description, value):
-        self.name = name
-        self.year = year
-        self.description = description
-        self.value = value
-        
-coins = [
-    Coin('Morgan Silver Dollar Cull', 1889, 'Vintage Silver dollar', 31.99),
-    Coin('Liberty Seated Dollar VG', 1871, 'Liberty seated dollar', 449.99),
-    Coin('Draped Bust Dollar VG-8 NGC', 1799, 'Rare Draped Bust Dollar with 13 stars', 1649),
-]
-
+from .models import Coin
 
 # Create your views here.
 def home(request):
@@ -23,4 +9,5 @@ def about(request):
     return render(request, 'about.html')
 
 def coins_index(request):
-  return render(request, 'coins/index.html', { 'coins': coins })
+    coins = Coin.objects.all()
+    return render(request, 'coins/index.html', { 'coins': coins })
