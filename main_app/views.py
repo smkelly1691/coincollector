82a751a7dc5a1d3426df55fbc8db6b232059d996
smkelly1691 <skelly1691@gmail.com>
2022-10-23 23:13:58 -0700

    adds index view and data

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Coin: 
+    def __init__(self, name, year, description, value):
+        self.name = name
+        self.year = year
+        self.description = description
+        self.value = value
+        
+coins = [
+    Coin('Morgan Silver Dollar Cull', 1889, 'Vintage Silver dollar', 31.99),
+    Coin('Liberty Seated Dollar VG', 1871, 'Liberty seated dollar', 449.99),
+    Coin('Draped Bust Dollar VG-8 NGC', 1799, 'Rare Draped Bust Dollar with 13 stars', 1649),
+]
+
 
 # Create your views here.
 def home(request):
@@ -8,3 +21,6 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def coins_index(request):
+  return render(request, 'coins/index.html', { 'coins': coins })
