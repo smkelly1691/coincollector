55d407867d8953ef8c160d65e1b8fd1c890fa4f5
smkelly1691 <skelly1691@gmail.com>
2022-10-25 13:47:32 -0700

    adds appraisal model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+TYPES = (
+    ('L1', 'Personal Research'),
+    ('L2', 'Informal Dealer Appraisal'),
+    ('L3', 'Numismatic Guaranty Corporation Appraisal')
+)
+
 # Create your models here.
 class Coin(models.Model):
     name = models.CharField(max_length=100)
@@ -13,3 +19,16 @@ class Coin(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'coin_id': self.id})
+    
+class Appraisal(models.Model):
+    date = models.DateField('Appraisal Date')
+    type = models.CharField(
+        max_length=2,
+        choices=TYPES,
+        default=TYPES[0][0]
+    )
+    
+    coin = models.ForeignKey(Coin, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.get_type_display()} on {self.date}"
